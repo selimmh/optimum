3543d780a731a0e2a6a9a20724a8ebaf84d8fb86
selimmh <selim.mht@gmail.com>
2022-03-11 18:23:24 +0200

    search filter

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,6 +2,9 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+// icons
+import { AiOutlineInfoCircle, AiOutlineSetting } from "react-icons/ai";
+
 // api functions
 import { deleteUser, getallUsers } from "../../utils/api";
 
@@ -12,15 +15,16 @@ import AddUser from "./AddUser";
 const headers = [
   "#",
   "Active",
-  "First",
-  "Last",
+  "Name",
+  // "Last",
   "Email",
-  "Password",
+  // "Password",
   "Role",
-  "Gender",
-  "Birthday",
-  "Nation",
-  "Actions",
+  // "Gender",
+  // "Birthday",
+  // "Nation",
+  "",
+  "",
 ];
 
 // main function
@@ -52,6 +56,17 @@ function Users() {
     !formOpen ? setformOpen(true) : setformOpen(false);
   };
 
+  // search filter
+  const [query, setQuery] = useState("");
+  console.log(query);
+
+  // more details
+
+  const [isOpen, setOpen] = useState(false);
+  const toggleMore = (id) => {
+    !isOpen ? setOpen(true) : setOpen(false);
+  };
+
   // all renders
   return (
     // page container
@@ -63,6 +78,19 @@ function Users() {
       >
         {formOpen ? <span>Close</span> : <span>Add new user</span>}
       </button>
+
+      <button className="mb-10 border-2  border-gray-800 py-2 px-4 rounded-md hover:bg-gray-800 hover:text-white ">
+        Filter
+      </button>
+
+      <input
+        placeholder="Search"
+        type="text"
+        name=""
+        id=""
+        className="border p-2 focus:border-red-500"
+        onChange={(e) => setQuery(e.target.value)}
+      />
       {formOpen ? (
         <div className="py-10 w-full">
           <AddUser />
@@ -81,66 +109,137 @@ function Users() {
               ))}
             </tr>
           </thead>
-          {user.map((data) => (
-            <tbody className="border-b hover:bg-gray-200 transition-all">
-              <td className="px-5 py-3 text-sm whitespace-nowrap">{data.id}</td>
-              <td className="px-5 py-3 text-sm whitespace-nowrap">
-                {data.active}
-                {data.active == "true" ? (
-                  <div className="bg-green-400 h-3 w-3 rounded-full" />
-                ) : (
-                  <div className="bg-red-400 h-3 w-3 rounded-full" />
-                )}
-              </td>
-              <td className="px-5 py-3 text-sm whitespace-nowrap">
-                {data.firstname}
-              </td>
-              <td className="px-5 py-3 text-sm whitespace-nowrap">
-                {data.lastname}
-              </td>
-              <td className="px-5 py-3 text-sm whitespace-nowrap">
-                {data.email}
-              </td>
-              <td className="px-5 py-3 text-sm whitespace-nowrap">
-                {data.password}
-              </td>
-              <td className="px-5 py-3 text-sm whitespace-nowrap">
-                {data.role}
-              </td>
-              <td className="px-5 py-3 text-sm whitespace-nowrap">
-                {data.gender}
-              </td>
-              <td className="px-5 py-3 text-sm whitespace-nowrap">
-                {data.birthday !== "" ? data.birthday : <p>Not set</p>}
-              </td>
-              <td className="px-5 py-3 text-sm whitespace-nowrap">
-                {data.nation !== "" ? data.nation : <p>Not set</p>}
-              </td>
-              <td className="space-x-2 px-5 py-3 text-sm whitespace-nowrap">
-                <button
-                  className="border-gray-800 text-xs border px-2 py-1 rounded-sm shadow-md hover:scale-105 transition-all"
-                  onClick={() => history(`/edituser/${data.id}`)}
-                >
-                  Edit
-                </button>
-                <button
-                  className="border-gray-800 text-xs border px-2 py-1 rounded-sm shadow-md hover:scale-105 transition-all"
-                  onClick={() => deleteData(data.id)}
-                >
-                  Del
-                </button>
-                <button
-                  onClick={() => history(`/assignDesk/${data.id}`)}
-                  className="border-gray-800 text-xs border px-2 py-1 rounded-sm shadow-md hover:scale-105 transition-all"
-                >
-                  Assign
-                </button>
-                <button className="border-gray-800 text-xs border px-2 py-1 rounded-sm shadow-md hover:scale-105 transition-all">
-                  De-Assign
-                </button>
-              </td>
-            </tbody>
-          ))}
+          {/* {user.map((data) => ( */}
+          {user
+            .filter(
+              (data) =>
+                data.firstname.toLowerCase().includes(query) ||
+                data.lastname.toLowerCase().includes(query) ||
+                data.email.toLowerCase().includes(query)
+            )
+            .map((data) => (
+              <>
+                <tbody className="border-b hover:bg-gray-200 transition-all">
+                  <td className="px-5 py-3 text-sm whitespace-nowrap">
+                    {data.id}
+                  </td>
+                  <td className="px-5 py-3 text-sm whitespace-nowrap">
+                    {/* {data.active} */}
+                    {data.active == "true" ? (
+                      <span class="flex h-3 w-3 relative">
+                        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+                        <span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+                      </span>
+                    ) : (
+                      <span class="flex h-3 w-3 relative">
+                        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+                        <span class="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+                      </span>
+                    )}
+                  </td>
+                  <td className="px-5 py-3 text-sm whitespace-nowrap">
+                    {data.firstname} {data.lastname}
+                  </td>
+                  {/* <td className="px-5 py-3 text-sm whitespace-nowrap group relative">
+                    {data.lastname}
+                    <div className="absolute -top-12 left-0 right-0 border-2 p-5 scale-0 group-hover:scale-100">
+                      info info info
+                    </div>
+                  </td> */}
+                  <td className="px-5 py-3 text-sm whitespace-nowrap">
+                    {data.email}
+                  </td>
+                  {/* <td className="px-5 py-3 text-sm whitespace-nowrap">
+                    {data.password}
+                  </td> */}
+                  <td className="px-5 py-3 text-sm whitespace-nowrap">
+                    {data.role}
+                  </td>
+
+                  <td className="px-5 py-3 text-sm whitespace-nowrap relative group cursor-context-menu">
+                    <AiOutlineInfoCircle className="text-2xl" />
+                    <div className="flex flex-col absolute right-[80%] -top-[50%] z-10 w-fit h-fit border-2 px-4 pt-2 scale-0 text-white bg-gray-700 rounded-md group-hover:scale-100 transition-all duration-300 ">
+                      <p>Gender: {data.gender}</p>
+                      <p>
+                        Birthday:{" "}
+                        {data.birthday !== "" ? (
+                          data.birthday
+                        ) : (
+                          <span>Not set</span>
+                        )}
+                      </p>
+                      <p>
+                        Nationality:{" "}
+                        {data.nation !== "" ? (
+                          data.nation
+                        ) : (
+                          <span>Not set</span>
+                        )}
+                      </p>
+                    </div>
+                  </td>
+                  {/* <td className="px-5 py-3 text-sm whitespace-nowrap">
+                    {data.gender}
+                  </td> */}
+                  {/* <td className="px-5 py-3 text-sm whitespace-nowrap">
+                    {data.birthday !== "" ? data.birthday : <p>Not set</p>}
+                  </td> */}
+                  {/* <td className="px-5 py-3 text-sm whitespace-nowrap">
+                    {data.nation !== "" ? data.nation : <p>Not set</p>}
+                  </td> */}
+                  <td className="px-5 py-3 text-sm whitespace-nowrap relative group cursor-context-menu">
+                    <AiOutlineSetting className="text-2xl" />
+                    <div className="-right-[50%] -top-[150%] align-center justify-center gap-2 w-48 flex flex-wrap absolute z-10 h-fit border-2 px-4 pt-2 scale-0 bg-gray-700 rounded-md group-hover:scale-100 transition-all duration-300">
+                      <button
+                        onClick={() => history(`/assignDesk/${data.id}`)}
+                        className="border-gray-800 text-xs border px-2 py-1 rounded-sm shadow-md hover:scale-105 transition-all bg-gray-100 hover:bg-gray-300"
+                      >
+                        Assign
+                      </button>
+                      <button className="border-gray-800 text-xs border px-2 py-1 rounded-sm shadow-md hover:scale-105 transition-all bg-gray-100 hover:bg-gray-300">
+                        De-Assign
+                      </button>
+                      <br />
+                      <button
+                        className="border-gray-800 text-xs border px-2 py-1 rounded-sm shadow-md hover:scale-105 transition-all bg-gray-100 hover:bg-gray-300"
+                        onClick={() => history(`/edituser/${data.id}`)}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="border-gray-800 text-xs border px-2 py-1 rounded-sm shadow-md hover:scale-105 transition-all bg-red-400 hover:bg-red-500"
+                        onClick={() => deleteData(data.id)}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </td>
+                  {/* <td className="space-x-2 px-5 py-3 text-sm whitespace-nowrap ">
+                    <button
+                      className="border-gray-800 text-xs border px-2 py-1 rounded-sm shadow-md hover:scale-105 transition-all"
+                      onClick={() => history(`/edituser/${data.id}`)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="border-gray-800 text-xs border px-2 py-1 rounded-sm shadow-md hover:scale-105 transition-all"
+                      onClick={() => deleteData(data.id)}
+                    >
+                      Del
+                    </button>
+                    <button
+                      onClick={() => history(`/assignDesk/${data.id}`)}
+                      className="border-gray-800 text-xs border px-2 py-1 rounded-sm shadow-md hover:scale-105 transition-all"
+                    >
+                      Assign
+                    </button>
+                    <button className="border-gray-800 text-xs border px-2 py-1 rounded-sm shadow-md hover:scale-105 transition-all">
+                      De-Assign
+                    </button>
+                  </td> */}
+                </tbody>
+              </>
+            ))}
         </table>
       </div>
     </div>
